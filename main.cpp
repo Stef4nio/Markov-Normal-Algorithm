396a88f0dd54bd7fe39f702912e84f5f86fc090b
stef4nio <stef4nio@gmail.com>
2020-01-30 14:24:25 +0200

    Test commit.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include "Node.h"
+#include <cstring>
 
 using namespace std;
 
-void stringToLinkedList(Node<char>** list,char string[],int stringLength)
+void stringToLinkedList(Node<char>** list,char string[])
 {
     Node<char>* tempNextNode;
-    for(int i = stringLength; i > 0; i--)
+    for(int i = strlen(string); i > 0; i--)
     {
         tempNextNode = new Node<char>;
         tempNextNode->Data = string[i-1];
         tempNextNode->NextNode = *list;
         *list = tempNextNode;
     }
+}
+
+template <typename T>
+void AddRangeToEnd(Node<T>* ListToBeModified,Node<T>* ListToBeAdded)
+{
+    while(ListToBeModified==NULL)
+    {
+        ListToBeModified = ListToBeModified->NextNode;
+    }
+    *ListToBeModified = Node<T>();
+    ListToBeModified->NextNode = ListToBeAdded;
 }
 
 bool findAndReplace(Node<char>** list,char findString[],char replaceString[])
@@ -33,10 +45,14 @@ bool findAndReplace(Node<char>** list,char findString[],char replaceString[])
         }
         if(findString[charsMatched] == '\0')
         {
-            Node<char>* lastCharMatched = currPosition;
+            Node<char>* replaceList;
+            stringToLinkedList(&replaceList,findString);
+            AddRangeToEnd(replaceList,currPosition->NextNode);
+            return true;
         }
         currPosition = currPosition->NextNode;
     }
+    return false;
 }
 
 int main() {
@@ -45,7 +61,14 @@ int main() {
     char inputStr[255];
     input.getline(inputStr,255);
     Node<char>* string;
-    stringToLinkedList(&string,inputStr,input.gcount());
+    stringToLinkedList(&string,inputStr);
+
+    Node<char>* testList1;
+    Node<char>* testList2;
+    stringToLinkedList(&testList1,"lol");
+    stringToLinkedList(&testList2,"kek");
+    AddRangeToEnd(testList1,testList2);
+    cout<<"adding complete";
 
     return 0;
 }
